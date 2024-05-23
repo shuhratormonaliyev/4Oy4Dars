5741d734392b4ec47cef5aa1e94a0d31002e1ccd
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-05-24 01:56:08 +0500

    pen

---
+++ js/index.js
@@ -0,0 +1,115 @@
+//  1 - MISOL
+
+// let t = +prompt('t ni kiriting');
+// let T = +prompt('T ni kiriting');
+
+// if (0 <= t && t <= 23) {
+//     console.log(t);
+// } else {
+//     console.log(error);
+// }
+// if (1 <= T && T <= 1000) {
+//     console.log(T);
+// } else {
+//     console.log(errors);
+// }
+
+// let y = (t + T) % 24;
+
+// console.log(y);
+
+// 2 - MISOL
+
+// let celsius = +prompt('celsius ni kiriting');
+
+// if (celsius < -273.15) {
+//     console.log("min qiymat shu!");
+// }
+// if (celsius > 1000) {
+//     console.log('max qiymati!');
+// }
+// const kelvin = celsius + 273.15;
+// const fahrenheit = celsius * 1.80 + 32.00;
+
+// console.log(kelvin.toFixed(5));
+// console.log(fahrenheit.toFixed(5));
+
+// 3- MISOL
+
+// let N = +prompt('N ni kiriting');
+
+// let K = +prompt('K ni kiriting');
+
+// if (N > 0 && K > 0) {
+//     console.log("TORI SON");
+// } else {
+//     console.log("error");
+// }
+// const valiyoshi = N / (K - 1);
+// const aliyoshi = valiyoshi + N;
+
+// console.log(valiyoshi);
+// console.log(aliyoshi);
+
+// 4 - misol
+
+// let n = +prompt('n kg tarvuz');
+// let a = +prompt('a tarvuz suvi');
+// let b = +prompt('b kamaygan holati');
+
+// const Boshida = n * (a / 100);
+
+// const quruqmodda = n - Boshida;
+
+// const Keyingisi = quruqmodda * (b / 100);
+
+// const tarvuz = quruqmodda + Keyingisi;
+
+// console.log(tarvuz.toFixed(5));
+
+// 5 - misol
+
+// let N = +prompt('qavariq kopburchakning ichki burchaglarini kiriting');
+
+// if (N > 3) {
+//     console.log('min 4 max 100 kiriting');
+// }
+// const yigindi = (N - 2) * 180;
+
+// console.log(yigindi);
+
+// 6 - MISOL
+
+// let = +prompt('Soat kiriting');
+// let = +prompt('daqiqani kiriting');
+// if (0 <= Soat && Daqiqa  <= 60) {
+//     Daqiqa = Soat * 60 ;
+//     Daqiqa %= 1440;
+
+// } else {
+//     console.log('erore');
+// }
+// let Daqiqa = Minut % 60;
+// let Soat = Math.floor(Minut / 60);
+// console.log(Daqiqa);
+// console.log(Soat);
+
+// 7 - misol
+
+// let = prompt(
+//   "sardorning buvisining yoshiga B va Sardorning oz yoshiga S deb qaraymiz X yillik vaqt otgach"
+// );
+// let = +prompt("B buvisini yoshini kiriting");
+// let = +prompt("S Sardorning yoshini kiriting");
+// let x = B - 2 * S;
+// if (x < 0) {
+//   console.log("-1");
+// } else {
+//   console.log("Yarim yoshiga yetadi");
+// }
+
+
+
+
+// 8 -MISOL 
+
